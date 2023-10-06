be2e4d0193c472966d79aba5f22641f6bf737ab3
Isna Ayu Muarofah <58203346+isnaayu@users.noreply.github.com>
2023-10-06 06:16:59 -0700

    Add files via upload

---
+++ CoffeeMachine/index.js
@@ -0,0 +1,24 @@
+import { coffeStock as stock, isCoffeeMachineReady } from "./state.js";
+
+//const {coffeStock, isCoffeeMachineReady} = require("./state.js");
+
+console.log(stock);
+console.log(isCoffeeMachineReady);
+
+// const makeCoffee = (type, miligrams) => {
+//     if(coffeStock[type] >= miligrams){
+//         console.log("Kopi berhasil dibuat");
+//     }else {
+//         console.log("Biji Kopi Habis");
+//     }
+// };
+
+// makeCoffee("robusta", 80);
+
+// console.log("Menyalakan mesin kopi");
+// console.log("Menggiling biji kopi");
+// console.log("Memanaskan Air");
+// console.log("Mencampurkan Air dan Kopi");
+// console.log("Menuangkan Kopi ke dalam gelas");
+// console.log("Menuangkan susu ke dalam gelas");
+// console.log("Kopi Anda sudah sia[p!");
---
+++ CoffeeMachine/state.js
@@ -0,0 +1,12 @@
+const coffeStock = {
+    arabica: 100,
+    robusta: 250,
+    liberica: 200
+}
+
+const isCoffeeMachineReady = true;
+
+export {coffeStock, isCoffeeMachineReady};
+//module.exports = {coffeStock, isCoffeeMachineReady};
+
+console.log(module);
