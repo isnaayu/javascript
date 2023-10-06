a572775e69941ae0dca61dd48ed1d2d728ad3b6b
Isna Ayu Muarofah <58203346+isnaayu@users.noreply.github.com>
2023-10-06 06:22:30 -0700

    Add files via upload

---
+++ play-with-npm/index.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+
+// const myArray = [1, 2, 3, 4];
+
+// let sum = 0;
+
+// for (let i = 0; i < myArray.length; i++) {
+//     sum += myArray[i];
+// }
+
+
+// let sum = myArray.reduce((prev, curr) => {
+//     return prev + curr;
+// })
+
+// const sum = _.sum(myArray);
+
+const name = 'dicoding';
+const language = 'javascript';
+
+<!-- --!>
+// console.log(sum);
+console.log(`hello ${name}. welcome ${language}`);
